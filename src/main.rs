7a49ecf47b87fa90963fd8851f897b52d897a171
LinkWu <wjianl@qq.com>
2022-08-02 16:40:05 +0800

    test code for codecov

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![crate_name = "purs"]
 #![crate_type = "bin"]
 
+use purs::pseudorandom::middle_square_factory;
+
 fn main() {
+    let mut gen = middle_square_factory(1123);
+    println!("{}", gen());
 }
